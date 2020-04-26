632f834e3d7bd6e524c63697a9e973c2fabc992a
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-26 18:45:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+ [] 
 end
 
 def create_an_array
-  
+  ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << "c"
 end
 
 def add_element_to_start_of_array(array, element)
